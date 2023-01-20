fcb652f2fcd08d60a8612539c61f912f33283e00
Johann <johann.agria@outlook.com>
2023-01-20 11:15:13 -0300

    inital commit

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,22 @@
+import styles from './Cabecalho.module.css';
+import { Link } from 'react-router-dom';
+import logo from './Logo.png';
+import CabecalhoLink from '../../components/CabecalhoLink';
+
+export default function Cabecalho() {
+    return (
+        <header className={styles.cabecalho}>
+            <Link to="./">
+                <img src={logo} alt="Logo do cinetag" />
+            </Link>
+            <nav>
+                <CabecalhoLink url="./">
+                    Home
+                </CabecalhoLink>
+                <CabecalhoLink url="./Favoritos">
+                    Favoritos
+                </CabecalhoLink>
+            </nav>
+        </header>
+    )
+}
